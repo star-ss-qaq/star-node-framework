c2383794c3bcab6768d7892be510f78db70e8e74
star-ss-qaq <597577939@qq.com>
2026-06-30 12:45:34 +0800

    fix: 规范

--- packages/core/src/utils.ts
+++ packages/core/src/utils.ts
@@ -1,4 +1,6 @@
-export function initMetadata(obj: any, key: string, defalutValue: any = {}) {
+export function initMetadata<T extends {} = any>(obj: any, key: string): T;
+export function initMetadata<T>(obj: any, key: string, defalutValue: T): T;
+export function initMetadata(obj: any, key: string, defalutValue = {} as any) {
 	if (!obj[key]) {
 		Object.defineProperty(obj, key, {
 			enumerable: false,
