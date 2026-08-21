7e63c1c49a5259d7633572251efd8d31307a09df
star-ss-qaq <597577939@qq.com>
2026-08-21 19:25:37 +0800

    fix(dev-env): 解决终端退出顺序问题并增加防重复打开处理

--- packages/dev-env/activate.js
+++ packages/dev-env/activate.js
@@ -1,14 +1,31 @@
-import { execSync, spawnSync } from "child_process";
+#!/usr/bin/env node
+import { execSync, spawn, spawnSync } from "child_process";
 import { userInfo } from "os";
 import { dirname, join } from "path";
 import { fileURLToPath } from "url";
 
-const tag = "\x1b[36m(SF-DEV)\x1b[0m"
+if (process.env.SF_DEV) {
+	console.warn("你似乎已经在开发环境了");
+	process.exit();
+}
+
+function spawn1(...arg) {
+	return new Promise((resolve, reject) => {
+		const p = spawn(...arg);
+		p.once("error", reject);
+		p.once("spawn", resolve);
+	});
+}
+
+const tag = "\x1b[36m(SF-DEV)\x1b[0m";
 
 const t = [
 	{
 		main: ["bash", "sh"],
-		args: ['-c',`exec bash --init-file <(echo "export PS1=\\"${tag}\\\\\\\\w$\\"")`],
+		args: [
+			"-c",
+			`exec bash --init-file <(echo "export PS1=\\"${tag}\\\\\\\\w$\\"")`,
+		],
 	},
 	{
 		main: [
@@ -29,9 +46,9 @@ const t = [
 
 const nodePath = process.execPath;
 const path = fileURLToPath(import.meta.url);
-const fakeNodePath = join(dirname(path),'fake-node');
-const viteNodeJS = join(dirname(path),'fake-node/vite-node.js');
-// const viteConfig = 
+const fakeNodePath = join(dirname(path), "fake-node");
+const viteNodeJS = join(dirname(path), "fake-node/vite-node.js");
+// const viteConfig =
 
 let flag = false;
 
@@ -39,19 +56,23 @@ for (var i = 0; i < t.length; i++) {
 	const cmd = t[i];
 	for (var j = 0; j < cmd.main.length; j++) {
 		try {
-			const handler = spawnSync(cmd.main[j], cmd.args, {
+			const handler = await spawn1(cmd.main[j], cmd.args, {
 				stdio: "inherit",
 				env: {
 					...process.env,
 					PATH: `${fakeNodePath};${process.env.PATH}`,
-                    REL_NODE_PATH: nodePath,
+					REL_NODE_PATH: nodePath,
 					FAKE_NODE_JS: viteNodeJS,
+					SF_DEV: 1,
 				},
 			});
-            if(!handler.error){
-                flag = true;
-                break;
-            }
+			flag = true;
+
+			process.on("SIGINT", () => {});
+			handler.on("exit", () => {
+				process.exit();
+			});
+			break;
 		} catch {}
 	}
 	if (flag) break;
