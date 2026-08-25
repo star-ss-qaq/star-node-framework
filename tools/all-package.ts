6edb88549fc45e77fe523ad60a82000c63539e2b
star-ss-qaq <597577939@qq.com>
2026-08-25 17:54:01 +0800

    chore: 增加醒目信息缓存

--- tools/all-package.ts
+++ tools/all-package.ts
@@ -1,14 +1,32 @@
 import { execSync } from "child_process";
-import { join } from "path";
+import { mkdirSync, readFileSync, writeFileSync } from "fs";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
 
-export function loadProjects() {
-	const projects: { name: string; version: string; location: string }[] =
-		JSON.parse(
-			execSync("npx lerna list --long --all --json", {
-				encoding: "utf-8",
-				stdio: "pipe",
-			}),
-		);
+interface ProjectInfo {
+	name: string;
+	version: string;
+	location: string;
+}
+export function loadProjects(): ProjectInfo[] {
+	const filePath = join(
+		dirname(fileURLToPath(import.meta.url)),
+		"..",
+		"node_modules",
+		".sf-tmp",
+		"projects.json",
+	);
+	try {
+		return JSON.parse(readFileSync(filePath, "utf-8"));
+	} catch {}
+	const projects: ProjectInfo[] = JSON.parse(
+		execSync("npx lerna list --long --all --json", {
+			encoding: "utf-8",
+			stdio: "pipe",
+		}),
+	);
+	mkdirSync(dirname(filePath), { recursive: true });
+	writeFileSync(filePath, JSON.stringify(projects));
 	return projects;
 }
 
