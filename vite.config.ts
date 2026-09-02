ce48d7e779aadecacdefe875f1b0e80b5d741fc4
star-ss-qaq <597577939@qq.com>
2026-09-02 20:24:54 +0800

    chore: 调整根目录配置，复用现有逻辑而不是使用之前的测试方法进行ts转换

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,40 @@
 process.env.NODE_ENV = "";
 import { defineConfig } from "vitest/config";
-import { deepkitType } from "@deepkit/vite";
-import { readdirSync } from "fs";
-import { join } from "path";
-import { execSync } from "child_process";
-import { transformFile } from "./helper.js";
 import { getProjectAliasObj } from "./tools/all-package.js";
+import { withTransform } from "./packages/ts-helper/src/transform-helper.js";
+import { transformer } from "@deepkit/type-compiler";
+import {
+	// @ts-ignore
+	Debug,
+} from "typescript";
 
-// const projects: { name: string; version: string; location: string }[] =
-// 	JSON.parse(
-// 		execSync("npx lerna list --long --all --json", {
-// 			encoding: "utf-8",
-// 			stdio: "pipe",
-// 		}),
-// 	);
 export default defineConfig({
 	plugins: [
 		// deepkitType({ compilerOptions: { sourceMap: true } }),
+		// {
+		// 	name: "deepkit-type",
+		// 	enforce: "pre",
+		// 	transform: transformFile,
+		// },
 		{
-			name: "deepkit-type",
+			name: "ts",
 			enforce: "pre",
-			transform: transformFile,
+			transform(code: string, id: string) {
+				Debug.setAssertionLevel(0); // 临时修复deepkit会在一些重载方法上报错，暂时不知道具体原因
+				if (id.endsWith("ts")) {
+					const data = withTransform(
+						code,
+						[
+							(context) => {
+								const t = transformer(context);
+								return (node) => t.transformSourceFile(node);
+							},
+						],
+						id,
+					);
+					return data;
+				}
+			},
 		},
 	],
 	test: {
